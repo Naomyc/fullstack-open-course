1140910b4ef542882dc2ff7d61e4e833489be29a
Chepngetich Naomy 39807 <naomy.chepngetich@edu.riveria.fi>
2025-12-15 14:43:00 +0200

    exercise 2.5 finished

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,36 +1,6 @@
-const Header=({name})=>{
-  return <h1>{name}</h1>
-}
-const Part=({part})=>{
-  return(
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-const Total=({parts})=>{
-  const total=parts.reduce((s,p)=> s + p.exercises,0)
-  return(
-    <p> total of {total} exercises</p>
-  )
-}
-const Content=({parts})=>{
-  return (
-    <div>
-      {parts.map(part=>(
-        <Part key={part.id} part ={part} />
-      ))}
-    </div>
-  )
-}
-const Course=({course})=>{
-  return(
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total  parts={course.parts} />
-      
-    </div>
-  )
-}
+
+import Course from "./components/Course"
+
 
 const App = () => {
   const courses = [{
