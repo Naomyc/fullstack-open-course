c940d6ce2a23e89d2fb24acb9cd2bb7ee6d1a4ce
Chepngetich Naomy 39807 <naomy.chepngetich@edu.riveria.fi>
2025-12-18 11:21:34 +0200

    exercise 2.6 phonebook step1

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,58 +1,50 @@
-
-import Course from "./components/Course"
-
+import { useState } from 'react'
 
 const App = () => {
-  const courses = [{
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name:'Redux',
-        exercises:11,
-        id: 4
-      }
-    ]
-  },{
-        name:'Node.js',
-        id: 2,
-        parts:[
-          {
-            name:'Routing',
-            exercises:3,
-            id:1
-          },
-          {
-            name:'Middlewares',
-            exercises:7,
-            id:2
-          }
-        ]
-      
-      }
-  
-    ]
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas',
+      id:1
+     }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const newPerson=(event)=>{
+    event.preventDefault()
+    const newObject={
+      name:newName,
+      id: persons.length +1
+    }
+    setPersons(persons.concat(newObject))
+    setNewName('')
+  }
+
+  const handlePersonChange=(event)=>{
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
   return (
-  <div>
-    {courses.map(course=>(
-      <Course key={course.id} course={course} />
-    ))}
-  </div>)
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={newPerson}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handlePersonChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person=>(
+          <li key={person.id}>
+            {person.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default App
